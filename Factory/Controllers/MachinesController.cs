a122a071f17ce511667be8529a0900d34da5701b
Caroline Cerussi <caroceru@gmail.com>
2022-08-11 16:58:00 -0700

    add rest of files and upload to remote

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Factory.Controllers
+{
+	public class MachinesController : Controller
+	{
+		private readonly FactoryContext _db;
+
+		public MachinesController(FactoryContext db)
+		{
+			_db = db;
+		}
+		public ActionResult Index()
+		{
+			return View(_db.Machines.ToList());
+		}
+		public ActionResult Create()
+		{
+		ViewBag.EId = new SelectList(_db.Engineers, "EId", "EName");
+		return View();
+		}
+	[HttpPost]
+	public ActionResult Create(Machine machine, int EId)
+	{
+		_db.Machines.Add(machine);
+		_db.SaveChanges();
+		if (EId != 0)
+		{
+			_db
+				.EngineerMachine
+				.Add(new EngineerMachine()
+				{EId = EId, MId = machine.MId});
+					_db.SaveChanges();
+		}
+		return RedirectToAction("Index");
+	}
+	public ActionResult Details (int id)
+	{
+		var thisMachine =
+		_db 
+		.Machines 
+		.Include (machine=> machine.JoinEntities)
+		.ThenInclude(join => join.Engineer)
+		.FirstOrDefault(machine => machine.MId == id);
+	return View(thisMachine);
+	}
+	public ActionResult Edit(int id)
+	{
+		var thisMachine =
+		 _db.Machines.FirstOrDefault(machine => machine.MId == id);
+		ViewBag.EId = new SelectList(_db.Engineers, "EId", "EName");
+		return View(thisMachine);
+	}
+    [HttpPost]
+    public ActionResult Edit(Machine machine, int EId)
+    {
+      if (EId != 0 && _db.EngineerMachine.FirstOrDefault(_d => _d.EId == EId && _d.MId == machine.MId) == null)
+      {
+        _db
+            .EngineerMachine
+            .Add(new EngineerMachine
+            { EId = EId, MId = machine.MId });
+        _db.SaveChanges();
+      }
+
+      _db.Entry(machine).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+
+		}
+  [HttpPost]
+	public ActionResult Delete(int id)
+	{
+		var thisMachine =
+		_db.Machines.FirstOrDefault(machine => machine.MId == id);
+		return View(thisMachine);
+	}
+	[HttpPost, ActionName("Delete")]
+	public ActionResult DeleteConfirmed(int id)
+	{
+		var thisMachine = 
+		_db.Machines.FirstOrDefault(machine => machine.MId == id);
+		_db.Machines.Remove(thisMachine);
+		_db.SaveChanges();
+		return RedirectToAction("Index");
+	}
+	public ActionResult AddE(int id)
+  {
+      var thisMachine =
+          _db.Machines.FirstOrDefault(machine => machine.MId == id);
+      ViewBag.EId = new SelectList(_db.Engineers, "EId", "EName");
+      return View(thisMachine);
+    }
+
+    [HttpPost]
+    public ActionResult AddE(Machine machine, int EId)
+    {
+      if (EId != 0)
+      {
+        _db
+            .EngineerMachine
+            .Add(new EngineerMachine()
+            { EId = EId, MId = machine.MId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Index");
+    }
+	}
+
+	}
---
+++ Factory/Models/EngineerMachine.cs
@@ -0,0 +1,14 @@
+
+namespace Factory.Models
+{
+	public class EngineerMachine
+	{
+		public int EngineerMachineId {get;set;}
+
+		public int MId {get;set;}
+		public virtual Machine Machine {get;set;}
+
+		public virtual Engineer Engineer {get;set;}
+		public int EId {get;set;}
+	}
+}
